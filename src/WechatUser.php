d695e233780c2eb4e8586486ec3bfe619373e1b2
ez <susheng@miinno.com>
2022-08-15 12:27:15 +0800

    releases 1.0

---
+++ src/WechatUser.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace bestyii\easywechat;
+
+use yii\base\Component;
+
+/**
+ * Class WechatUser
+ * @package bestyii\easywechat
+ */
+class WechatUser extends Component
+{
+    /**
+     * @var string
+     */
+    public $id;
+    /**
+     * @var string
+     */
+    public $nickname;
+    /**
+     * @var string
+     */
+    public $name;
+    /**
+     * @var string
+     */
+    public $email;
+    /**
+     * @var string
+     */
+    public $avatar;
+    /**
+     * @var array
+     */
+    public $original;
+    /**
+     * @var \Overtrue\Socialite\AccessToken
+     */
+    public $token;
+    /**
+     * @var string
+     */
+    public $provider;
+
+    /**
+     * @return string
+     */
+    public function getOpenId()
+    {
+        return isset($this->original['openid']) ? $this->original['openid'] : '';
+    }
+}
